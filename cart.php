396f7f199ef5b17e2846b5a377800692c9dc450b
Trong Nghia <nghiatrong060505@gmail.com>
2025-04-08 23:36:03 +0700

    bo file css vao css cho gon

--- cart.php
+++ cart.php
@@ -39,7 +39,7 @@ if (isset($_POST['add_to_cart'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="cart.css">
+    <link rel="stylesheet" href="css/cart.css">
 </head>
 
 <body>
