fad3bcef88411b9f650e9006c88de437acdcc1a5
L0ngg <hohoanglong2508@gmail.com>
2025-04-28 13:50:37 +0700

    Chinh sua thong ke

--- view/OrderView.php
+++ view/OrderView.php
@@ -1,4 +1,5 @@
 <h2 style="margin-top: 20px">Danh sách đơn hàng của khách hàng <?php echo $orders[0]['customer_name']; ?></h2>
+<div class="back-btn"><i class="fa-solid fa-2x fa-arrow-left"></i></div>
 <div class="table-container">
     <?php
     if (!empty($orders)) {
@@ -14,7 +15,7 @@
                 <td>{$order['order_id']}</td>
                 <td>{$order['OrderDate']}</td>
                 <td>{$order['total']}</td>
-                <td><a href=''>Chi tiết</a></td>
+                <td><button class=\"detail-btn\" value=\"{$order['order_id']}\" onclick=\"showOrderDetail(this)\">📄 Chi tiết</button></td>
               </tr>";
         }
         echo "</table>";
@@ -23,3 +24,48 @@
     }
     ?>
 </div>
+<div id="order-detail-popup">
+    <div id="order-detail-content">
+        <h2>Chi tiết đơn hàng</h2>
+        <table id="detail-table">
+            <thead>
+                <tr>
+                    <th>Tên sản phẩm</th>
+                    <th>Số lượng</th>
+                    <th>Đơn giá</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+        <button id="close-btn" onclick="hideOrderDetail()">Đóng</button>
+    </div>
+</div>
+
+<script>
+    document.querySelector('.back-btn').addEventListener("click", (e) => {
+        window.location.href = 'admin.php?page=thongke';
+        console.log('hihihi');
+
+    })
+</script>
+
+<style>
+    .detail-btn {
+        border: none;
+        /* width: 72px;
+        height: 30px; */
+        cursor: pointer;
+        border-radius: 5px;
+        border: 1px solid #BDC3C7;
+        margin: 2px;
+        background: white;
+        padding: 8px;
+    }
+
+    .detail-btn:hover {
+        background-color: #69b7ff;
+        color: white;
+    }
+</style>
